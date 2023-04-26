5177e4c688dec50fb34506d21e68f5cc82248092
LeoMoulin <leomoulin125@gmail.com>
2023-04-26 13:58:00 +0200

    Fill the JSON with the levels 11-30 data :)

--- src/main/java/code/projetinfo/AppGame.java
+++ src/main/java/code/projetinfo/AppGame.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-
+import code.projetinfo.normalBlocks.*;
 public class AppGame extends Application {
     @Override
     public void start(Stage stage) throws IOException {
