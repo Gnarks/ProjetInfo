c6e27835b50607f8ed72c3cd5b33d4bef65d9a18
LeoMoulin <leomoulin125@gmail.com>
2023-04-30 20:42:09 +0200

    Fix the Gradle for the unit testing. (remove the exclude in hansolo dependency field and change unit test engine). :-)

---
+++ src/test/java/code/projetinfo/LevelTest.java
@@ -0,0 +1,16 @@
+package code.projetinfo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LevelTest {
+
+    @Test
+    public void isPlacable() {
+    }
+
+    @Test
+    public void place() {
+    }
+}
