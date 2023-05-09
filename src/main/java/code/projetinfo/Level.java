7fdb2094a7b059c4a05de6fb2632f26db1f033a6
LeoMoulin <leomoulin125@gmail.com>
2023-05-09 11:49:25 +0200

    Move levels.json in the good place

--- src/main/java/code/projetinfo/Level.java
+++ src/main/java/code/projetinfo/Level.java
@@ -12,7 +12,7 @@ public class Level {
     private ImageBlock[] blocks;
     private int placed = 0;
     private String name;
-    private String pathname = System.getProperty("user.dir")+"<src<main<resources<levels.json";
+    private String pathname = System.getProperty("user.dir")+"<src<main<resources<code<projetinfo<levels.json";
     private final File f = new File(pathname.replaceAll("<", "\\"+System.getProperty("file.separator")));
     private final ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
     public Level(String name, Cases grid, ImageBlock[] blocs){
