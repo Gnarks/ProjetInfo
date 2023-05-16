40513d691bf4f5654bf7f268c4ab5f94d7467a7f
Maxime <231026@umons.ac.be>
2023-05-16 11:41:40 +0200

    docu Position

--- src/main/java/code/projetinfo/Position.java
+++ src/main/java/code/projetinfo/Position.java
@@ -12,16 +12,18 @@ public class Position implements Comparable<Position>{
     private final double x;
     private final double y;
 
-    /**
-     * @param x  the object's horizontal position
-     * @param y  the object's vertical position
+    /**Default constructor for Position.
+     * Sets (x,y) as a Position.
+     *
+     * @param x  the object's horizontal position.
+     * @param y  the object's vertical position.
      */
     public Position(double x,double y){
         this.x = x;
         this.y = y;
     }
 
-    /**
+     /**Returns the object's horizontal position
      *
      * @return the object's horizontal position
      */
@@ -29,7 +31,7 @@ public class Position implements Comparable<Position>{
         return x;
     }
 
-    /**
+     /**Returns the object's vertical position
      *
      * @return the object's vertical position
      */
@@ -37,14 +39,27 @@ public class Position implements Comparable<Position>{
         return y;
     }
 
+    /** Returns if the specified Position is equal to the current Position.
+     *
+     * @param other the other Position to check with.
+     * @return if the other Position and the current Position have the same data.
+     */
     public boolean equals(Position other){
         return other.getX() == this.getX() && other.getY() == this.getY();
     }
 
+    /**Returns a string representation.
+     *
+     * @return a String representing.
+     */
     public String toString(){
         return String.format("(%s, %S)",x,y);
     }
 
+    /** Returns the cloned position.
+     *
+     * @return a new Position with the same data.
+     */
     @Override
     public Position clone(){
         return new Position(x,y);
